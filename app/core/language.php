6458a7e70ceab98dd06526fe98323068e3173fd3
grkvnko <gorkaveng@yandex.ru>
2016-09-19 15:40:58 +1000

    Refactoring languages/translate classes.

---
+++ app/core/language.php
@@ -0,0 +1,32 @@
+<?php
+
+class Language 
+{
+    private static $languages = ['РУС' => 'ru', 'ENG' => 'en'];
+
+    public static function getLanguages()  
+    {
+        return self::$languages;
+    }
+
+    public static function getSelectedLanguage()
+    {
+        if(isset($_SESSION['lang'])) {
+            $selected_lang = mb_substr($_SESSION['lang'], 0, 3);
+            if(in_array($selected_lang, self::getLanguages())) {
+                return $selected_lang;
+            }
+        }
+        return Config::getDefaultLanguage();
+    }
+
+    public static function setCurrentLanguage($lang)
+    {
+        $lang = mb_substr($lang, 0, 3);
+        if(in_array($lang, self::getLanguages(), true)) {
+            $_SESSION['lang'] = $lang;
+            return true;
+        }
+        return false;
+    }
+}
