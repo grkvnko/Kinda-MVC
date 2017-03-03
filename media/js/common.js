830b0f6413004d0d79236288554cd13857b29f21
GREG <gorkaveng@yandex.ru>
2017-03-04 02:26:40 +0300

    убрал blocksit с главной страницы, сделал CSS колонки.

--- media/js/common.js
+++ media/js/common.js
@@ -1,19 +1,7 @@
-function getColumnCount() {
-	var windowWidth = $(window).width();
-	if (windowWidth < 1000) {
-		return 1;
-	} else if (windowWidth < 1500) {
-		return 2;
-	} else {
-		return 3;
-	}
-}
 
-function go_BlocksIT() {
-	$('#post_prev').BlocksIt({ numOfCol: getColumnCount(), offsetX: 0, offsetY: 0 });
-}
+$(document).ready(function () {
+	$('.justified-gallery img').css("height", 100);
 
-function go_justifiedGallery() {
 	$('.justified-gallery').justifiedGallery({
 		maxRowHeight : 100,
 		rowHeight: 100,
@@ -27,29 +15,5 @@ function go_justifiedGallery() {
 		margins: 5,
 		lastRow: 'nojustify'
 	});
-}
 
-function setTimers() {
-	go_BlocksIT();
-	for (x = 100; x <= 1000; x = x + 300)
-		setTimeout(go_BlocksIT, x);
-}
-
-$(window).load( function() {
-	setTimers();
 });
-
-$(window).focus( function() {
-	go_BlocksIT();
-});
-
-$(document).ready(function () {
-	$('.justified-gallery img').css("height", 100);
-	go_BlocksIT();
-	go_justifiedGallery();
-});
-
-$(window).resize(function() {
-	setTimers();
-});
-
